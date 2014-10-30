4c0fe4b5ec25cc6795783702509ca32c92a7482f
Start <4544242@qq.com>
2014-10-30 17:40:03 +0800

    1

--- Ancun/AncunYL/src/com/ancun/yzb/layout/DialContentView.java
+++ Ancun/AncunYL/src/com/ancun/yzb/layout/DialContentView.java
@@ -5,9 +5,7 @@ import com.ancun.core.BaseScrollContent;
 import com.ancun.yzb.R;
 
 public class DialContentView extends BaseScrollContent{
-	
 	public DialContentView(BaseActivity activity) {
 		super(activity, R.layout.module_scroll_dial);
 	}
-
 }
