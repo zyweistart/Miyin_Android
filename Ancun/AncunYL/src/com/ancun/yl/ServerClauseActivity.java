bd42072ef7511b2c97701c587cd3aa0da6d85fce
Start <4544242@qq.com>
2014-11-10 16:13:44 +0800

    1

--- Ancun/AncunYL/src/com/ancun/yl/ServerClauseActivity.java
+++ Ancun/AncunYL/src/com/ancun/yl/ServerClauseActivity.java
@@ -1,6 +1,8 @@
 package com.ancun.yl;
 
+import start.core.AppConstant;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import com.ancun.core.BaseActivity;
 /**
@@ -9,11 +11,17 @@ import com.ancun.core.BaseActivity;
  */
 public class ServerClauseActivity extends BaseActivity {
 	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_serverclause);
-		setMainHeadTitle("服务条款");
-    }
+	private static final String WEBURL = "file:///android_asset/html/serverclause.html";
 
+	private WebView mWebView;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tip);
+		setMainHeadTitle("服务条款");
+		mWebView=(WebView)findViewById(R.id.wvcontent);
+		mWebView.getSettings().setDefaultTextEncodingName(AppConstant.ENCODE);
+		mWebView.loadUrl(WEBURL);
+	}
 }
