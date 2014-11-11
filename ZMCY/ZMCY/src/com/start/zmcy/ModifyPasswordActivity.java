f5093660ac79f24f22812a389fbc49f317e7f1b9
Start <4544242@qq.com>
2014-11-11 17:20:31 +0800

    1

---
+++ ZMCY/ZMCY/src/com/start/zmcy/ModifyPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.start.zmcy;
+
+import android.os.Bundle;
+
+import com.start.core.BaseActivity;
+
+
+public class ModifyPasswordActivity extends BaseActivity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+ 	}
+}
