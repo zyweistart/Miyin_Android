616a15ec586c502b1930dac0c41935eb2ad789eb
Start <4544242@qq.com>
2014-11-25 20:52:33 +0800

    1

--- ZMCY/ZMCY/src/com/start/service/HttpServer.java
+++ ZMCY/ZMCY/src/com/start/service/HttpServer.java
@@ -5,13 +5,12 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.ProtocolVersion;
 import org.apache.http.client.HttpClient;
@@ -204,8 +203,8 @@ public class HttpServer {
 		String[] arrs={Config.ACCESS_TOKEN,timestamp,nonce};
 		//升序排列
 		Arrays.sort(arrs);
-		String signature=arrs[0]+arrs[1]+arrs[2];
-		String url=String.format(AppContext.getInstance().getServerURL(), this.mUrl,SHA1(signature),timestamp,nonce);
+		String signature= DigestUtils.shaHex(arrs[0]+arrs[1]+arrs[2]);
+		String url=String.format(AppContext.getInstance().getServerURL(), this.mUrl,signature,timestamp,nonce);
 		HttpPost post = new HttpPost(url);
 		try {
 			post.addHeader("format", "json");
@@ -357,31 +356,5 @@ public class HttpServer {
 		
 	}
 	
-	public String SHA1(String inStr) {
-        MessageDigest md = null;
-        String outStr = null;
-        try {
-            md = MessageDigest.getInstance("SHA-1");     //选择SHA-1，也可以选择MD5
-            byte[] digest = md.digest(inStr.getBytes());       //返回的是byet[]，要转化为String存储比较方便
-            outStr = bytetoString(digest);
-        }catch(NoSuchAlgorithmException nsae) {
-            nsae.printStackTrace();
-        }
-        return outStr;
-    }
-	
-	public String bytetoString(byte[] digest) {
-        String str = "";
-        String tempStr = "";
-        for (int i = 1; i < digest.length; i++) {
-            tempStr = (Integer.toHexString(digest[i] & 0xff));
-            if (tempStr.length() == 1) {
-                str = str + "0" + tempStr;
-            } else {
-                str = str + tempStr;
-            }
-        }
-        return str.toLowerCase();
-    }
 	
 }
