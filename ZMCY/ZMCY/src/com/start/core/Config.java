0d677162be39e169490d8b5d21f306dfb39b8500
Start <4544242@qq.com>
2014-12-11 17:52:55 +0800

    1

--- ZMCY/ZMCY/src/com/start/core/Config.java
+++ ZMCY/ZMCY/src/com/start/core/Config.java
@@ -13,17 +13,17 @@ public class Config {
 	public static final String QQAPPID = "100424468";
 	public static final String QQAPPKEY = "c7394704798a158208a74ab60104f0ba";
 
-	// TODO:传统光源
-	public static final String NLIGHTSOURCEURL="http://api.020leader.com/59/qiye_list2.html";
-	// TODO: LED光源
-	public static final String LIGHTSOURCEURL="http://www.163.com";
-	// 商城
-	public static final String MallURL=AppContext.getInstance().getServerURL()+"/38/shop_list.html ";
-	// TODO: 展会
-	public static final String ExhibitionURL="http://www.sina.com";
-	// 专家自荐
-	public static final String EXPERTSURL=AppContext.getInstance().getServerURL()+"/63/zijian.html";
+	//传统光源
+	public static final String NLIGHTSOURCEURL=AppContext.getInstance().getServerURL()+"/59/qiye_list2.html";
+	//ED光源
+	public static final String LIGHTSOURCEURL=AppContext.getInstance().getServerURL()+"http://www.163.com";
+	//商城
+	public static final String MallURL=AppContext.getInstance().getServerURL()+"/ios_shop_list.html ";
+	//展会
+	public static final String ExhibitionURL=AppContext.getInstance().getServerURL()+"/ios_zhanhui.html";
+	//专家自荐
+	public static final String EXPERTSURL=AppContext.getInstance().getServerURL()+"/ios_zhuanjia_list.html";
 	// 应用
-	public static final String APPURL=AppContext.getInstance().getServerURL()+"/40/yingyong.html";
+	public static final String APPURL=AppContext.getInstance().getServerURL()+"/ios_yingyong.html";
 
 }
