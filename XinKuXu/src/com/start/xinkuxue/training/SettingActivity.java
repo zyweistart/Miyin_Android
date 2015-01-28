079cc9ae793d0a3cac5d02ef6d220a6abf386155
Start <4544242@qq.com>
2015-01-28 10:27:09 +0800

    1

---
+++ XinKuXu/src/com/start/xinkuxue/training/SettingActivity.java
@@ -0,0 +1,19 @@
+package com.start.xinkuxue.training;
+
+import android.os.Bundle;
+
+import com.start.core.BaseActivity;
+
+/**
+ * 设置
+ * @author zhenyao
+ *
+ */
+public class SettingActivity extends BaseActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+}
